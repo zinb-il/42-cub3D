5ff9de3921e57697a6483397cf1631fec77d953f
zinb9419 <97052963+zinb9419@users.noreply.github.com>
2022-12-29 17:31:30 +0100

    cub bonus

--- include_bonus/sprite_bonus.h
+++ include_bonus/sprite_bonus.h
@@ -6,13 +6,16 @@
 /*   By: ziloughm <ziloughm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/27 16:53:50 by ziloughm          #+#    #+#             */
-/*   Updated: 2022/12/27 19:27:34 by ziloughm         ###   ########.fr       */
+/*   Updated: 2022/12/29 17:27:46 by ziloughm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #	ifndef SPRITE_BONUS_H
 # define SPRITE_BONUS_H
 
-# define SPRITE ""
+# define SPRITE "../img_cub/sprite.xpm"
+# define DOOR1 "../img_cub/door_1.xpm"
+# define DOOR2  "../img_cub/door_2.xpm"
+# define DOOR3 "../img_cub/door_3.xpm"
 
 #	endif
