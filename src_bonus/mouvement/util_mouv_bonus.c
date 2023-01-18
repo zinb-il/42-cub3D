3ab8a9c449bd3828a02ea8ee73a4f51fece63dcd
emmaOa <93877957+emmaOa@users.noreply.github.com>
2023-01-18 19:21:56 +0100

    collisions wall

--- src_bonus/mouvement/util_mouv_bonus.c
+++ src_bonus/mouvement/util_mouv_bonus.c
@@ -3,18 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   util_mouv_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ziloughm <ziloughm@student.42.fr>          +#+  +:+       +#+        */
+/*   By: iouazzan <iouazzan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 13:49:40 by iouazzan          #+#    #+#             */
-/*   Updated: 2023/01/15 21:19:06 by ziloughm         ###   ########.fr       */
+/*   Updated: 2023/01/18 19:20:03 by iouazzan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include_bonus/cub3D_bonus.h"
 
+void	util_collisions(t_data *data, int x, int y)
+{
+	if (data->gm->map[x / 64][(int)data->pp_y / 64] != '1' && \
+		data->gm->map[x / 64][(int)data->pp_y / 64] != '2' && \
+		data->gm->map[x / 64][(int)data->pp_y / 64] != '3')
+			data->pp_x = data->n_p_x;
+	else if (data->gm->map[(int)data->pp_x / 64][y / 64] != '1' && \
+			data->gm->map[(int)data->pp_x / 64][y / 64] != '2' && \
+			data->gm->map[(int)data->pp_x / 64][y / 64] != '3')
+				data->pp_y = data->n_p_y;
+}
+
 void	collisions_wall(t_data *data, int x, int y)
 {
-	if (data->gm->map[x / 64][y / 64] != '1')
+	if (data->gm->map[x / 64][y / 64] != '1' && \
+		data->gm->map[x / 64][y / 64] != '2' && \
+		data->gm->map[x / 64][y / 64] != '3')
 	{
 		if (x / 64 != (((int)data->pp_x) / SIZE_WIN) \
 			&& y / 64 != ((int)data->pp_y / SIZE_WIN))
@@ -28,11 +42,6 @@ void	collisions_wall(t_data *data, int x, int y)
 	else
 	{
 		if ((int)data->pp_x != x && (int)data->pp_y != y)
-		{
-			if (data->gm->map[x / 64][(int)data->pp_y / 64] != '1')
-				data->pp_x = data->n_p_x;
-			else if (data->gm->map[(int)data->pp_x / 64][y / 64] != '1')
-				data->pp_y = data->n_p_y;
-		}
+			util_collisions(data, x, y);
 	}
 }
